bab52c991f60b3a8b0ba897d3f7cd2a33a47b5c7
skellythor <flammavindex@gmail.com>
2022-07-12 19:59:15 -0300

    new artwork

---
+++ index.js
@@ -0,0 +1,4 @@
+function setHalfVolume() {
+    var myAudio = document.getElementById("audio1");  
+    myAudio.volume = 0.5; //Changed this to 0.5 or 50% volume since the function is called Set Half Volume ;)
+}
